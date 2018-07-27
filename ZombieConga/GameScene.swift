f3f4ee5f0833c8da087c3fa2edeb0c79a1008d5d
Ma Xueyuan <machinemxy@me.com>
2018-07-27 23:42:21 +0900

    finish chapter1 sprites

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -10,10 +10,20 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    let zombie = SKSpriteNode(imageNamed: "zombie1")
+    
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        
+        //add background
         let background = SKSpriteNode(imageNamed: "background1")
         background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.zPosition = -1
         addChild(background)
+        
+        //add the zombie
+        zombie.position = CGPoint(x: 400, y: 400)
+        zombie.setScale(2.0)
+        addChild(zombie)
     }
 }
