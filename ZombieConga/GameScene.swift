c3403542e3c41a22ac9ed58c2d2731f2ff25abcc
Ma Xueyuan <machinemxy@me.com>
2018-07-30 23:38:40 +0900

    Chapter 2 Phase 4 まで done

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,6 +11,10 @@ import GameplayKit
 
 class GameScene: SKScene {
     let zombie = SKSpriteNode(imageNamed: "zombie1")
+    var lastUpdateTime: TimeInterval = 0
+    var dt: TimeInterval = 0
+    let zombieMovePointsPerSec: CGFloat = 480.0
+    var velocity = CGPoint.zero
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
@@ -23,7 +27,87 @@ class GameScene: SKScene {
         
         //add the zombie
         zombie.position = CGPoint(x: 400, y: 400)
-        zombie.setScale(2.0)
+        //zombie.setScale(2.0)
+        
         addChild(zombie)
+        
+        //gesture recognize
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tapRecognizer)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        
+        move(sprite: zombie, velocity: velocity)
+        boundsCheckZombie()
+    }
+    
+    func move(sprite: SKSpriteNode, velocity: CGPoint) {
+        let amoutToMove = CGPoint(x: velocity.x * CGFloat(dt), y: velocity.y * CGFloat(dt))
+        sprite.position = CGPoint(x: sprite.position.x + amoutToMove.x, y: sprite.position.y + amoutToMove.y)
+    }
+    
+    func moveZombieToward(location: CGPoint) {
+        let offset = CGPoint(x: location.x - zombie.position.x, y: location.y - zombie.position.y)
+        let length = sqrt(Double(offset.x * offset.x + offset.y * offset.y))
+        let direction = CGPoint(x: offset.x / CGFloat(length), y: offset.y / CGFloat(length))
+        velocity = CGPoint(x: direction.x * zombieMovePointsPerSec, y: direction.y * zombieMovePointsPerSec)
+    }
+    
+    func sceneTouched(touchLocation: CGPoint) {
+        moveZombieToward(location: touchLocation)
+    }
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        guard let touch = touches.first else {
+//            return
+//        }
+//        let touchLocation = touch.location(in: self)
+//        sceneTouched(touchLocation: touchLocation)
+//    }
+//
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        guard let touch = touches.first else {
+//            return
+//        }
+//        let touchLocation = touch.location(in: self)
+//        sceneTouched(touchLocation: touchLocation)
+//    }
+    
+    @objc func handleTap(recognizer: UIGestureRecognizer) {
+        let viewLocation = recognizer.location(in: self.view)
+        let touchLocation = convertPoint(fromView: viewLocation)
+        sceneTouched(touchLocation: touchLocation)
+    }
+    
+    func boundsCheckZombie() {
+        let bottomLeft = CGPoint.zero
+        let topRight = CGPoint(x: size.width, y: size.height)
+        
+        if zombie.position.x <= bottomLeft.x {
+            zombie.position.x = bottomLeft.x
+            velocity.x = -velocity.x
+        }
+        
+        if zombie.position.x >= topRight.x {
+            zombie.position.x = topRight.x
+            velocity.x = -velocity.x
+        }
+        
+        if zombie.position.y <= bottomLeft.y {
+            zombie.position.y = bottomLeft.y
+            velocity.y = -velocity.y
+        }
+        
+        if zombie.position.y >= topRight.y {
+            zombie.position.y = topRight.y
+            velocity.y = -velocity.y
+        }
     }
 }
